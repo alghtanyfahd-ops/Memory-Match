b48ecc75aac37dfb009bbf72d7eeeb5b219188d4
alghtanyfahd-ops <alghtanyfahd@gmail.com>
2026-07-06 09:32:09 +0300

    api/createPayment.js

--- api/createPayment.js
+++ api/createPayment.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         metadata: {}
       })
     });
-
+    
     const data = await response.json();
     return res.status(200).json(data);
 

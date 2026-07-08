dfe618b66ab56693fb63073e522cd4fe219291c2
alghtanyfahd-ops <alghtanyfahd@gmail.com>
2026-07-08 16:07:01 +0300

    Fix formatting of validation key response

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export default {
     // Pi Domain Validation
     if (request.method === "GET" && url.pathname === "/validation-key.txt") {
       return new Response(
-        "ce0c45ed75eee0b02640c20964969d7394bc1c22bf7a06bdf625fb3afd137ef3ce7b85202c75379a8f8d11a700af7e6ca1f882e02d44c784297a46ff633b4988 ",
+        "ce0c45ed75eee0b02640c20964969d7394bc1c22bf7a06bdf625fb3afd137ef3ce7b85202c75379a8f8d11a700af7e6ca1f882e02d44c784297a46ff633b4988",
         {
           headers: {
             "Content-Type": "text/plain"

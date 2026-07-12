6726b7531230ac8c1319b06852f201cff14b438a
alghtanyfahd-ops <alghtanyfahd@gmail.com>
2026-07-12 07:39:37 +0300

    Implement drawTest function for canvas rendering
    Added drawTest function to render a wheel on canvas.

--- wheel.js
+++ wheel.js
@@ -2,12 +2,63 @@ console.log("wheel.js loaded");
 
 function openWheel(){
 
-    alert("العجلة تعمل ✅");
+    document
+    .getElementById("wheelModal")
+    .classList
+    .remove("hidden");
+
+    drawTest();
 
 }
 
 function closeWheel(){
 
-    alert("إغلاق");
+    document
+    .getElementById("wheelModal")
+    .classList
+    .add("hidden");
+
+}
+
+function drawTest(){
+
+    const canvas =
+    document.getElementById("wheelCanvas");
+
+    const ctx =
+    canvas.getContext("2d");
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    ctx.fillStyle = "red";
+
+    ctx.beginPath();
+
+    ctx.arc(
+        160,
+        160,
+        120,
+        0,
+        Math.PI * 2
+    );
+
+    ctx.fill();
+
+    ctx.fillStyle = "white";
+
+    ctx.font = "30px Arial";
+
+    ctx.textAlign = "center";
+
+    ctx.fillText(
+        "🎡",
+        160,
+        170
+    );
 
 }
